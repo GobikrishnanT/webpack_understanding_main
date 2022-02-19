5c0fab064278c1311c58b8dd9a2f294f3808c726
Gobikrishnan <gobikrishnantj@gmail.com>
2022-02-19 11:34:34 +0530

    Setted up the formatting things to work

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+(() => {
+  console.log('=============================='),
+    console.log('Hello all'),
+    console.log('Imorted Cars ---\x3e SCORPIOomni'),
+    console.log('==============================');
+})();
